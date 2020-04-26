64e161e66ca2e037826f4975cafb6aab8bd56171
palomino19 <jpalomino09@googlemail.com>
2020-04-25 23:20:58 -0700

    trblshoot(gameReducer.js): change deck name to try and get build to work

--- src/redux/reducers/gameReducer.js
+++ src/redux/reducers/gameReducer.js
@@ -1,11 +1,11 @@
 import * as ACTIONS from "../actions";
-import deck from "../../data/deck.json";
+import newDeck from "../../data/deck.json";
 import _ from "lodash";
 import produce from "immer";
 
 const initialState = {
   activePlayer: 1,
-  deck: _.shuffle(deck),
+  deck: _.shuffle(newDeck),
 };
 
 const gameReducer = produce((state = initialState, action) => {
